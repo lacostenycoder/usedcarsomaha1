097349022f001b2196dbff46a51771ded150f58b
Josh Greenwood <joshua.t.greenwood@gmail.com>
2014-08-15 13:52:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UsedCarsOmaha::Application.config.secret_key_base = 'f63801247bb1b835073ce47114c0f5e286aeed5f19338f2d51119e42abec509429b64c0d9c38f529efd2a1777a0e5f6526b6ea26796a112dff4d66019a1bc5e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UsedCarsOmaha::Application.config.session_store :cookie_store, key: '_used_cars_omaha_session'
