efbc558c1e407c976c0fec87c145fdb26142c71d
Josh Greenwood <joshua.t.greenwood@gmail.com>
2014-08-26 12:04:29 -0400

    Adds phone number field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826155729) do
+ActiveRecord::Schema.define(version: 20140826160340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140826155729) do
     t.datetime "updated_at"
     t.boolean  "admin"
     t.integer  "dealership_id"
+    t.string   "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
