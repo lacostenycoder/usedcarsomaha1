ce59a0b90524d749e74a00c1178b86738ad7cb54
Joshua Loper <el3ctro@Joshuas-MacBook-Air.local>
2014-10-01 12:18:33 -0500

    Destroy records not found in CSV file.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,8 @@
 class Vehicle < ActiveRecord::Base
 require 'smarter_csv'
 
-default_scope { where(type: "Used") }
+default_scope { where("type = 'Used' AND not year = 2015") }
+# default_scope { not(year: 2015) }
 # belongs_to :dealership
 
 # Necessary for using the "type column"
@@ -15,21 +16,18 @@ self.inheritance_column = nil
     n = SmarterCSV.process(file.path, {file_encoding: 'iso-8859-1', row_sep: :auto, :key_mapping => {:id => :original_id}}) do |array|
 
       vehicle = Vehicle.where(original_id: array.first[:original_id])
-
+      original_ids << array.first[:original_id]
       if vehicle.count == 1
         vehicle.first.update_attributes(array.first)
       elsif vehicle.count > 1
         raise "There is more than 1 vehicle with the original_id of #{array.first[:original_id]}"
-      elsif vehicle.count == 0
-        vehicle.update(:type => "Sold")
-        vehicle.save
       else
         #attempt to avoid a typing error by converting the price to an integer before creating
         array[:price] = array[:price].to_i
         array[:mileage] = array[:mileage].to_i
         Vehicle.create( array.first )
       end
-
     end
+    Vehicle.where('original_id not in (?)',original_ids).delete_all
   end
 end
